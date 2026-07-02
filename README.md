3e850da7b5b753787192f43ab51025c212556f1e
Himanshu Kumar Dubey <hkd05101999@gmail.com>
2026-07-02 22:57:37 +0530

    Implement transaction storage and view functionality

---
+++ README.md
@@ -0,0 +1,15 @@
+# Personal Finance Manager
+
+A command-line Personal Finance Manager built in Python.
+
+## Features
+
+- Add Income
+- Add Expense
+- View Transactions
+
+## Technologies
+
+- Python
+- Git
+- GitHub
--- main.py
+++ main.py
@@ -7,8 +7,20 @@ def add_transaction(transaction_type: str):
                 }
     return transaction_detail
 
-def view_transaction():
-    return ("You chose View Transactions")
+def save_transaction(transaction: dict,transaction_list: list):
+    transaction_list.append(transaction)
+    
+    
+def view_transaction(transaction_list: list):
+    formatted_result=[]
+    for transactions in transaction_list:
+        transaction_type=transactions['type']
+        amount=transactions['amount']
+        description=transactions['description']
+        formatted_result.append(f"Transaction Type:{transaction_type} Amount:{amount} Description:{description}")
+    return formatted_result
+
+    
 
 def display_menu():
     print(("\n===== Personal Finance Manager ====="))
@@ -16,22 +28,42 @@ def display_menu():
     return user_choice
     
 
-def main():
-    
-    user_choice=display_menu()
 
-    if user_choice==1:
-        income_added=(add_transaction("income"))
-        print(income_added)
-    elif user_choice==2:
-        expense_added=(add_transaction("expense"))
-        print(expense_added)
-    elif user_choice==3:
-        print(view_transaction())
-    elif user_choice==4:
-        print("Goodbye!")
-    else:
-        print("Invalid menu option.")
+def main():
+    transaction_list=[]
+    while True:
+        user_choice=display_menu()
+        transact_typ=""
+        if user_choice==1:
+            transact_typ="Income"
+        elif user_choice==2:
+            transact_typ="Expense"
+        elif user_choice==3:
+            formatted=view_transaction(transaction_list)
+            if formatted!=[]:
+                for item in formatted:
+                    print(item)
+            else:
+                print("No Transactions to show.")        
+                    
+        elif user_choice==4:
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid menu option.")
+        
+        if user_choice in(1,2):
+            transaction=add_transaction(transact_typ)
+            save_transaction(transaction,transaction_list)
+    
+    #transaction_list=[{'type': 'Income', 'amount': 500, 'description': 'abc'}, {'type': 'Expense', 'amount': 433, 'description': 'dsa'}]
+    #formatted=view_transaction(transaction_list)
+    #for item in formatted:
+        #print(item)
+    #    if formatted!=[]:
+    #        print(item)
+        
+    #print("No Transactions to show.") 
 
 if __name__=="__main__":
     main()
