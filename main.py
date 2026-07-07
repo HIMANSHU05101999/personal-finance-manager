26d0e3320e564760f62ab6195a9dc57d8e43cd2f
Himanshu Kumar Dubey <hkd05101999@gmail.com>
2026-07-08 00:30:14 +0530

    refactor: simplify transaction workflow and improve menu structure feat: merge income and expense into unified transaction flow

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import json 
 from pathlib import Path
-def add_transaction(transaction_type: str, date: datetime):
+def add_transaction(transaction_type: str, date: str):
     amount=int(input("Enter amount:"))
     description=input("Enter description:")
     transaction_detail={"date":date,
@@ -16,87 +16,95 @@ def save_transaction(transaction: dict,transaction_list: list, transaction_file:
     with open(transaction_file,"w") as trans_file:
         json.dump(transaction_list, trans_file, indent=4)
     
-    
-    
+
 def view_transaction(transaction_list: list):
-    formatted_result=[]
-    for transactions in transaction_list:
-        date=transactions['date']
-        transaction_type=transactions['type']
-        amount=transactions['amount']
-        description=transactions['description']
-        formatted_result.append(
-                        f"--------------------------\n"
-                        f"Date: {date}\n"
-                        f"Transaction Type: {transaction_type}\n"
-                        f"Amount: {amount}\n"
-                        f"Description: {description}\n"
-                        f"--------------------------"
-                                )
-    return formatted_result
+    if transaction_list:
+    
+        for transaction in transaction_list:
+            date=transaction['date']
+            transaction_type=transaction['type']
+            amount=transaction['amount']
+            description=transaction['description']
+    
+            print("--------------------------")
+            print(f"Date: {date}")
+            print(f"Transaction Type: {transaction_type}")
+            print(f"Amount: {amount}")
+            print(f"Description: {description}")
+            print(f"--------------------------")
+    
+    else:
+        print("No Transactions to Show!")
+
+
 
 def load_transaction(transaction_file: str):
     try:
         with open(transaction_file) as trans_file:
             database_transaction=json.load(trans_file)
-    
+            return database_transaction
+        
     except FileNotFoundError:
             data=[]
             with open(transaction_file,"w") as file:
                 json.dump(data, file, indent=4)
-
+                return data
     except json.JSONDecodeError:
             return [] 
-            
     
 def display_menu():
     print(("\n===== Personal Finance Manager ====="))
-    user_choice=int(input("1.Add Income\n2.Add Spends\n3.View Transaction\n4.Exit\nYour Choice: "))
+    user_choice=int(input("1.Add Transaction\n2.View Transaction\n3.Exit\nYour Choice: "))
     return user_choice
     
 def get_transaction_date():
     while True:
-        date_choice=int(input("Update Date Press 1 Or Continue with Today's Date Press 2: "))
+        date_choice=int(input("Press 1: Update Date\nPress 2: Continue with Today's Date\n Choose: "))
         if date_choice==1:
             date=input("Enter Date (DD.MM.YYYY): ")
-            date=str(datetime.strptime(date,'%d.%m.%Y'))
-            return date
+            date=datetime.strptime(date,'%d.%m.%Y')
+            return date.strftime("%d.%m.%Y")
         elif date_choice==2:
             return datetime.today().strftime('%d.%m.%Y')
         else:
             print("Invalid Choice Enter Again!!!")
+
+def transaction_type():
+    while True:
+        trans_type=int(input("Press 1: Income\nPress 2: Expense\n Choose:"))
+        if trans_type==1:
+            transact_typ="Income"
+            break
+        elif trans_type==2:
+            transact_typ="Expense"
+            break
+        else:
+            print("Invalid Entry")
+    return transact_typ
+
 
 def main():
     transaction_file=Path(__file__).parent/"transaction_data.json"
     transaction_list=load_transaction(transaction_file)
     while True:
         user_choice=display_menu()
-        transact_typ=""
+        trans_type=0
         if user_choice==1:
-            transact_typ="Income"
+            transact_typ=transaction_type()
+            date=get_transaction_date()
+            transaction=add_transaction(transact_typ,date)
+            save_transaction(transaction,transaction_list,transaction_file)
+
         elif user_choice==2:
-            transact_typ="Expense"
+            view_transaction(transaction_list)                   
         elif user_choice==3:
-            formatted=view_transaction(transaction_list)
-            if formatted!=[]:
-                for item in formatted:
-                    print(item)
-            else:
-                print("No Transactions to show.")        
-                    
-        elif user_choice==4:
             print("Goodbye!!!")
             break
         else:
             print("Invalid menu option.")
+
         
-        if user_choice in(1,2):
-            date=get_transaction_date()
-            transaction=add_transaction(transact_typ,date)
-            save_transaction(transaction,transaction_list,transaction_file)
-            
-            
-                
+    
 
 if __name__=="__main__":
     main()
